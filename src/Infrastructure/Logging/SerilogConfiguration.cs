a47c78e9ab28cc279541cff1596b9d629cc949da
Mikhail M <47109585+MikhailMasny@users.noreply.github.com>
2019-11-25 23:03:07 +0300

    Updated Serilog configuration mechanism.

--- src/Infrastructure/Logging/SerilogConfiguration.cs
+++ src/Infrastructure/Logging/SerilogConfiguration.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Masny.QRAnimal.Application.Models;
+using Masny.QRAnimal.Infrastructure.Extensions;
+using Microsoft.Extensions.Configuration;
 using Serilog;
 using Serilog.Core;
+using Serilog.Events;
 
 namespace Masny.QRAnimal.Infrastructure.Logging
 {
@@ -15,14 +18,43 @@ namespace Masny.QRAnimal.Infrastructure.Logging
         /// <returns>Конфигурация Seriog.</returns>
         public static Logger LoggerConfig()
         {
-            // UNDONE: Config для Docker
             var configuration = new ConfigurationBuilder()
                 .AddJsonFile("appsettings.json")
                 .Build();
 
-            var serilogConfig = new LoggerConfiguration()
-                .ReadFrom.Configuration(configuration)
+            var appSettingSection = configuration.GetSection("AppSettings");
+            var appSettings = appSettingSection.Get<AppSettings>();
+            var isDockerSupport = appSettings.IsDockerSupport;
+
+            var connectionString = configuration.GetConnectionString(isDockerSupport.ToDbConnectionString());
+            var tableName = "EventLog";
+
+            //var object1 = new SqlColumn { ColumnName = "OtherData", DataType = SqlDbType.NVarChar, DataLength = 64 };
+            //var object2 = new SqlColumn { ColumnName = "AnotherData", DataType = SqlDbType.NVarChar, DataLength = 64 };
+
+            //var columnOption = new ColumnOptions();
+            //columnOption.Store.Remove(StandardColumn.MessageTemplate);
+            //columnOption.Store.Remove(StandardColumn.Properties);
+
+            //columnOption.AdditionalColumns = new Collection<SqlColumn>();
+            //columnOption.AdditionalColumns.Add(object1);
+            //columnOption.AdditionalColumns.Add(object2);
+
+            var serilogConfig =
+                new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+                .MinimumLevel.Override("System", LogEventLevel.Warning)
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .WriteTo.MSSqlServer(connectionString,
+                                     tableName,
+                                     //columnOptions: columnOption,
+                                     //schemaName: "log",
+                                     autoCreateSqlTable: true)
                 .CreateLogger();
+
 
             return serilogConfig;
         }
