0a5b6dae0fb5043068c74359dfa4980959b6e445
Mikhail M <47109585+MikhailMasny@users.noreply.github.com>
2019-11-26 23:10:55 +0300

    Added NuGet packet MimeKit and base interface implementation.

--- src/Infrastructure/Services/EmailSender.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Masny.QRAnimal.Infrastructure.Services
-{
-    class EmailSender
-    {
-        // UNDONE: Реализовать на основе MimeKit
-    }
-}
---
+++ src/Infrastructure/Services/EmailSenderService.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Masny.QRAnimal.Application.Interfaces;
+
+namespace Masny.QRAnimal.Infrastructure.Services
+{
+    /// <summary>
+    /// Сервис для отправки сообщений по электронной почте.
+    /// </summary>
+    public class EmailSenderService : IMessageSender
+    {
+        /// <inheritdoc />
+        public Task SendMessageAsync(string recipient, string subject, string message)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
