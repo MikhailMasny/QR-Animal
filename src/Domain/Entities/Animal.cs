83f7f678eb4337ff2274a72d095af099217f8dae
Mikhail M <47109585+MikhailMasny@users.noreply.github.com>
2019-11-28 23:43:55 +0300

    Added animal entity.

---
+++ src/Domain/Entities/Animal.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Domain.Entities
+{
+    /// <summary>
+    /// Модель животного.
+    /// </summary>
+    public class Animal
+    {
+        /// <summary>
+        /// Идентификатор.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Идентификатор пользователя.
+        /// </summary>
+        public string UserId { get; set; }
+
+        /// <summary>
+        /// Вид (разновидность).
+        /// </summary>
+        public string Kind { get; set; }
+
+        /// <summary>
+        /// Порода.
+        /// </summary>
+        public string Breed { get; set; }
+
+        /// <summary>
+        /// Пол.
+        /// </summary>
+        public bool Gender { get; set; }
+
+        /// <summary>
+        /// Паспорт.
+        /// </summary>
+        public string Passport { get; set; }
+
+        /// <summary>
+        /// Дата рождения.
+        /// </summary>
+        public DateTime BirthDate { get; set; }
+
+        /// <summary>
+        /// Кличка.
+        /// </summary>
+        public string Nickname { get; set; }
+
+        /// <summary>
+        /// Особенности.
+        /// </summary>
+        public string Features { get; set; }
+    }
+}
