240849f5c570a36321efb884e850d3c5acd68480
Mikhail M <47109585+MikhailMasny@users.noreply.github.com>
2019-11-28 23:31:55 +0300

    Added QRCode entity.

---
+++ src/Domain/Entities/QRCode.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Domain.Entities
+{
+    /// <summary>
+    /// Модель QR кода.
+    /// </summary>
+    public class QRCode
+    {
+        /// <summary>
+        /// Идентификатор.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Идентификатор животного.
+        /// </summary>
+        public int AnimalId { get; set; }
+
+        /// <summary>
+        /// Данные для генерации.
+        /// </summary>
+        public string Code { get; set; }
+
+        /// <summary>
+        /// Дата создания.
+        /// </summary>
+        public DateTime Created { get; set; }
+    }
+}
