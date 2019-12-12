40005eb20eedab3706860697c24d945b9ea056f4
Mikhail M <47109585+MikhailMasny@users.noreply.github.com>
2019-12-13 00:25:58 +0300

    Changed some namespaces. Added references to Worker project. Added another one basic implementation of hosted service with scoped service.

--- src/Worker/Startup.cs
+++ src/Worker/Startup.cs
@@ -2,14 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Masny.QRAnimal.Infrastructure.Services;
 using Masny.QRAnimal.Worker;
+using Masny.QRAnimal.Worker.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace Worker
+namespace Masny.QRAnimal.Worker
 {
     public class Startup
     {
@@ -17,7 +19,8 @@ namespace Worker
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddHostedService<TimedHostedService>();
+            services.AddHostedService<ConsumeScopedServiceHostedService>();
+            services.AddScoped<IScopedProcessingService, ScopedProcessingService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
