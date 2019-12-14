a2ed6a9f0a5ea7de2b9cb9ea0a6cfe3233c57ec6
Mikhail M <47109585+MikhailMasny@users.noreply.github.com>
2019-12-14 15:38:52 +0300

    Added serilog support to worker hosted service.

--- src/Worker/Program.cs
+++ src/Worker/Program.cs
@@ -1,26 +1,41 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Masny.QRAnimal.Infrastructure.Logging;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Serilog;
+using System;
 
 namespace Masny.QRAnimal.Worker
 {
     public class Program
     {
+        private static readonly string url = "http://*:84";
+
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            Log.Logger = SerilogConfiguration.LoggerConfig();
+
+            try
+            {
+                Log.Information("Starting web host");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.UseUrls(url);
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseSerilog();
                 });
     }
 }
--- src/Worker/Startup.cs
+++ src/Worker/Startup.cs
@@ -24,12 +24,12 @@ namespace Masny.QRAnimal.Worker
             Environment = environment;
         }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             var appSettingSection = Configuration.GetSection("AppSettings");
             services.Configure<AppSettings>(appSettingSection);
+
+            // UNDONE: Рефакторинг
 
             // Добавлен Identity контекст.
             var isDockerSupport = appSettingSection.Get<AppSettings>().IsDockerSupport;
