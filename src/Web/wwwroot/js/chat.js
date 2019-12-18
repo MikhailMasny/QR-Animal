3dd3a7fa4e8735bde0a2981f854004790a9d54d2
Mikhail M <47109585+MikhailMasny@users.noreply.github.com>
2019-12-18 22:36:48 +0300

    Added css "li-without-dot" and etc in design for li.

--- src/Web/wwwroot/js/chat.js
+++ src/Web/wwwroot/js/chat.js
@@ -9,6 +9,7 @@ connection.on("ReceiveMessage", function (user, message) {
     var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
     var encodedMsg = user + ": " + msg;
     var li = document.createElement("li");
+    li.className = "shadow-sm p-3 bg-white rounded mb-2 li-without-dot";
     li.textContent = encodedMsg;
     document.getElementById("messagesList").appendChild(li);
     document.getElementById("messageInput").value = "";
