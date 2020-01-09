c9fbf72b934d9bac3fd5396f55fb7de2214154d8
Mikhail M <47109585+MikhailMasny@users.noreply.github.com>
2020-01-09 23:46:47 +0300

    Added basic controllers for WebAPI.

---
+++ src/Web/Controllers/WebAPI/AccountsController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Masny.QRAnimal.Web.Controllers.WebAPI
+{
+    public class AccountsController
+    {
+    }
+}
