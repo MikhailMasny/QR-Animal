6aa763cf4a90f544bfb38dfc3763ff5f24d5e263
Mikhail M <47109585+MikhailMasny@users.noreply.github.com>
2020-01-13 23:24:39 +0300

    Refactoring method GetAllPublicAnimalsAsync. Added method GetPublicAnimalAsync to get current animal. Added functionality for caching data.

--- src/Web/Controllers/HomeController.cs
+++ src/Web/Controllers/HomeController.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Localization;
 using System;
 using System.Globalization;
 
