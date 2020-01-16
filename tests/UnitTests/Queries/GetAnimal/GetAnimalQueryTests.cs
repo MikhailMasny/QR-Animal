1b8cd635ec3f784499165fbd8590d03db75e5fcc
Mikhail M <47109585+MikhailMasny@users.noreply.github.com>
2020-01-16 23:36:31 +0300

    Added some queries tests.

--- tests/UnitTests/Queries/GetAnimal/GetAnimalQueryTests.cs
+++ tests/UnitTests/Queries/GetAnimal/GetAnimalQueryTests.cs
@@ -1,39 +1,38 @@
 ﻿using Masny.QRAnimal.Application.CQRS.Queries.GetAnimal;
 using Masny.QRAnimal.Application.DTO;
 using Masny.QRAnimal.Domain.Enums;
-using Masny.QRAnimal.UnitTests;
 using Shouldly;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
 
-namespace UnitTests.Queries.GetAnimal
+namespace Masny.QRAnimal.UnitTests.Queries.GetAnimal
 {
     public class GetAnimalQueryTests : BaseTestsFixture
     {
         [Fact]
-        public async Task Handle_ReturnsCorrectAnimalDTO()
+        public async Task Handle_WhenIsAnotherUser_ReturnsCorrectAnimalDTO()
         {
             // Arrange
             var animal = new AnimalDTO
             {
                 Id = 1,
-                UserId = "QWERTY1234567890",
-                Kind = "Kind",
-                Breed = "Breed",
+                UserId = "QWERTY1234567890_One",
+                Kind = "Kind_One",
+                Breed = "Breed_One",
                 Gender = GenderTypes.None,
-                Passport = "1234567890QWERTY",
-                BirthDate = new DateTime(2000, 01, 01),
-                Nickname = "Nickname",
-                Features = "Features",
+                Passport = "1234567890QWERTY_One",
+                BirthDate = new DateTime(2001, 01, 01),
+                Nickname = "Nickname_One",
+                Features = "Features_One",
                 IsPublic = true
             };
 
             var query = new GetAnimalQuery
             {
                 Id = 1,
-                UserId = "QWERTY1234567890",
+                UserId = "QWERTY1234567890_One",
                 AnotherUser = false
             };
 
@@ -43,20 +42,87 @@ namespace UnitTests.Queries.GetAnimal
             var result = await handler.Handle(query, CancellationToken.None);
 
             // Assert
-            //result.ShouldBeOfType<AnimalDTO>();
+            result.ShouldBeOfType<AnimalDTO>();
 
-            //result.ShouldNotBeNull();
+            result.ShouldNotBeNull();
 
-            //result.Id.ShouldBe(animal.Id);
-            //result.UserId.ShouldBe(animal.UserId);
-            //result.Kind.ShouldBe(animal.Kind);
-            //result.Breed.ShouldBe(animal.Breed);
-            //result.Gender.ShouldBe(animal.Gender);
-            //result.Passport.ShouldBe(animal.Passport);
-            //result.BirthDate.ShouldBe(animal.BirthDate);
-            //result.Nickname.ShouldBe(animal.Nickname);
-            //result.Features.ShouldBe(animal.Features);
-            //result.IsPublic.ShouldBe(animal.IsPublic);
+            result.Id.ShouldBe(animal.Id);
+            result.UserId.ShouldBe(animal.UserId);
+            result.Kind.ShouldBe(animal.Kind);
+            result.Breed.ShouldBe(animal.Breed);
+            result.Gender.ShouldBe(animal.Gender);
+            result.Passport.ShouldBe(animal.Passport);
+            result.BirthDate.ShouldBe(animal.BirthDate);
+            result.Nickname.ShouldBe(animal.Nickname);
+            result.Features.ShouldBe(animal.Features);
+            result.IsPublic.ShouldBe(animal.IsPublic);
+        }
+
+        [Fact]
+        public async Task Handle_WhenIsNotAnotherUser_ReturnsCorrectAnimalDTO()
+        {
+            // Arrange
+            var animal = new AnimalDTO
+            {
+                Id = 1,
+                UserId = "QWERTY1234567890_One",
+                Kind = "Kind_One",
+                Breed = "Breed_One",
+                Gender = GenderTypes.None,
+                Passport = "1234567890QWERTY_One",
+                BirthDate = new DateTime(2001, 01, 01),
+                Nickname = "Nickname_One",
+                Features = "Features_One",
+                IsPublic = true
+            };
+
+            var query = new GetAnimalQuery
+            {
+                Id = 1,
+                UserId = string.Empty,
+                AnotherUser = true
+            };
+
+            // Act
+            var handler = new GetAnimalQuery.GetAnimalQueryHandler(Context, Mapper);
+
+            var result = await handler.Handle(query, CancellationToken.None);
+
+            // Assert
+            result.ShouldBeOfType<AnimalDTO>();
+
+            result.ShouldNotBeNull();
+
+            result.Id.ShouldBe(animal.Id);
+            result.UserId.ShouldBe(animal.UserId);
+            result.Kind.ShouldBe(animal.Kind);
+            result.Breed.ShouldBe(animal.Breed);
+            result.Gender.ShouldBe(animal.Gender);
+            result.Passport.ShouldBe(animal.Passport);
+            result.BirthDate.ShouldBe(animal.BirthDate);
+            result.Nickname.ShouldBe(animal.Nickname);
+            result.Features.ShouldBe(animal.Features);
+            result.IsPublic.ShouldBe(animal.IsPublic);
+        }
+
+        [Fact]
+        public async Task Handle_GivenInvalidId_ReturnsCorrectAnimalDTO()
+        {
+            // Arrange
+            var query = new GetAnimalQuery
+            {
+                Id = 99,
+                UserId = string.Empty,
+                AnotherUser = true
+            };
+
+            // Act
+            var handler = new GetAnimalQuery.GetAnimalQueryHandler(Context, Mapper);
+
+            var result = await handler.Handle(query, CancellationToken.None);
+
+            // Assert
+            result.ShouldBeNull();
         }
     }
 }
