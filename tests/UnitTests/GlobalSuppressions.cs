c491c4e9d6be6ee29960bf485eb5d3491565c76f
Mikhail M <47109585+MikhailMasny@users.noreply.github.com>
2020-01-16 23:57:14 +0300

    Some warnings are suppressed and disabled.

---
+++ tests/UnitTests/GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA1816:Dispose methods should call SuppressFinalize", Justification = "<Pending>", Scope = "member", Target = "~M:Masny.QRAnimal.UnitTests.BaseTestsFixture.Dispose")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1063:Implement IDisposable Correctly", Justification = "<Pending>", Scope = "member", Target = "~M:Masny.QRAnimal.UnitTests.BaseTestsFixture.Dispose")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1063:Implement IDisposable Correctly", Justification = "<Pending>", Scope = "type", Target = "~T:Masny.QRAnimal.UnitTests.BaseTestsFixture")]
