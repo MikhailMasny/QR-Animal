bc9814ba6fbe35f45368925bf43ea2fe40a4c779
Mikhail M <47109585+MikhailMasny@users.noreply.github.com>
2020-01-18 00:05:33 +0300

    Added reference to IntegrationTests project. Added necessary NuGet packages. Added basic class of WebTestFixture. Added implementation of WebApplicationFactory test.

---
+++ tests/IntegrationTests/HomeControllerIndex.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Masny.QRAnimal.IntegrationTests
+{
+    [Collection("Sequential")]
+    public class HomeControllerIndex : IClassFixture<WebTestFixture>
+    {
+        public HomeControllerIndex(WebTestFixture factory)
+        {
+            Client = factory.CreateClient();
+        }
+
+        public HttpClient Client { get; }
+
+        [Fact]
+        public async Task Return_HomePage_WithFooterInformation()
+        {
+            // Arrange & Act
+            var response = await Client.GetAsync("/");
+            response.EnsureSuccessStatusCode();
+            var stringResponse = await response.Content.ReadAsStringAsync();
+
+            // Assert
+            Assert.Contains("QR Animals &copy; 2019", stringResponse);
+        }
+    }
+}
